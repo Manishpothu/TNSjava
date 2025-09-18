b81c55730222947d697075c7839f20fe9661e634
Manish <manishpothu003@gmail.com>
2025-09-18 09:44:17 +0530

    firstcommit

---
+++ prime.java
@@ -0,0 +1,27 @@
+package com.tns.java;
+
+import java.util.Scanner;
+
+public class prime {
+	static boolean isPrime(int n) 
+	{
+		for(int i=2; i<=n/2; i++) 
+		{
+			if(n%i == 0) 
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(isPrime(n));
+		
+
+	}
+
+}
